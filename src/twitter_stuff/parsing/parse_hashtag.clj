882ab219d9023c8877c0269c0f54961faa6c40a7
Sam Raker <sam.raker@rga.com>
2014-11-16 23:47:18 -0500

    updates to parse_hashtag

--- src/twitter_stuff/parsing/parse_hashtag.clj
+++ src/twitter_stuff/parsing/parse_hashtag.clj
@@ -5,14 +5,16 @@
 
 (def bests (atom {}))
 
-(def word-freqs (parse-stream
-                 (io/reader "resources/lowered_freqs.json")))
+(def word-freqs (let [m1 (parse-stream (io/reader "resources/lowered_freqs.json"))
+		      m2 (parse-stream (io/reader "resources/freqs_invokeit.json"))]
+		  (apply merge (map #(apply hash-map %) (map (fn [k] [k (/ (+ (get m1 k 0) (get m2 k 0)) 2)]) (distinct (concat (keys m1) (keys m2))))))))
 
 (def oov (/ (count word-freqs)))
 
 (def subword-parser (insta/parser
                      "<S> = W+
                      W = #'\\w'+"))
+(defn parse-to-string [parse] (map #(apply str (rest %)) parse))
 
 (defn get-subwords [s] (doseq [parses (insta/parses subword-parser s)]
                         (map (fn [p] (apply str (rest p))) parses)))
@@ -23,11 +25,19 @@
   [(Math/pow (apply * freqs) (/ (count ss)))
    ss]))
 
+(defn get-score-arith [parse-str] 
+	(long (/ (apply + (map #(get word-freqs % 0) parse-str)) (count parse-str))))
+(defn get-score [parse-str]
+	(let [freqs (map #(* (get word-freqs % oov) (/ (count parse-str))) parse-str)]
+		(Math/pow (apply * freqs) (/ (count parse-str)))))
+
 (defn get-best-parse [s]
-  (if-let [curr-best (get bests s)]
-    curr-best
-    ([let best (last (apply sorted-map
-                   (reduce concat
-                       (pmap rank-freqs
-                             (get-subwords s)))))]
-        (swap! bests assoc s best))))
+  (if-let [curr-best (get @bests s)]
+    (first curr-best)
+    (do
+      (doseq [parse (insta/parses subword-parser s)]
+	(let [score (get-score (parse-to-string parse))]
+		(if (> score (second (get @bests s [nil 0])))
+			(swap! bests assoc s [(parse-to-string parse) score]))))
+      (first (get @bests s)))))
+
