0cfb5d4367b7fd57876415155e8649d21e9d25e4
Sam Raker <sam.raker@rga.com>
2014-11-23 22:33:17 -0500

    couch updates

--- src/twitter_stuff/utils/couch.clj
+++ src/twitter_stuff/utils/couch.clj
@@ -1,11 +1,15 @@
 (ns twitter-stuff.utils.couch
   (:require [com.ashafa.clutch :as clutch]
-            [cemerick.url :refer [url]]
+            [cemerick.url]
             [environ.core :refer [env]]
             [clojure.core.match :refer [match]]))
 
 
-(defn get-db [] (clutch/get-database "twitter"))
+(defn get-db [] (clutch/get-database (assoc (cemerick.url/url "http://127.0.0.1/"
+						 "twitter")
+					:port 5984
+					:username "swizzard"
+					:password "discworld")))
 
 (defn multi-update [old new]
   (if (nil? old)
